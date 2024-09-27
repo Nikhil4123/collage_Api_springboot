f98a116829b2404b1b9f660f9baaa37b6ff46a27
Nikhil4123 <nikhilsarak612w@gmail.com>
2024-09-27 19:47:58 +0530

    Removed all files from the repository

--- spring-boot-course-app-master/spring-boot-course-app-master/course-app/src/main/java/com/course/courseapp/model/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.course.courseapp.model;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends JpaRepository<Course, Integer>, JpaSpecificationExecutor<Course> {
-}
